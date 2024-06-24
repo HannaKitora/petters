ebc9af689ff1fe7c36dd7581a97385f79d8c0dcd
HannaKitora <h.wimper@gmail.com>
2024-06-24 14:32:23 +0000

    layout0624

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,18 +1,6 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
-  
-  # def search
-  #   @range = params[:range]
 
-  #   if @range == "User"
-  #     @users = User.looks(params[:search], params[:word])
-  #   elsif @range == "Pet"
-  #     @pets = Pet.looks(params[:search], params[:word])
-  #   elsif @range == "Event"
-  #     @events = Event.looks(params[:search], params[:word])
-  #   end
-  # end
-  
   def search
     @model = params[:model]
     @content = params[:content]
