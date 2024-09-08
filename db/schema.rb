cb9ce789f459624c4491dda5845e4b527b87a04f
HannaKitora <h.wimper@gmail.com>
2024-09-08 00:14:51 +0000

    20240907233747_addpet_id_to_kindsファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_07_233747) do
+ActiveRecord::Schema.define(version: 2024_09_07_235610) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2024_09_07_233747) do
     t.string "kind"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "pet_id"
   end
 
   create_table "notifications", force: :cascade do |t|
